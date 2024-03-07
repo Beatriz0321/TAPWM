a4618889267bfb77812d8dcd6ea8107cf69ea924
Beatriz Alamino <82543262+Beatriz0321@users.noreply.github.com>
2024-03-06 22:17:53 -0300

    Atividade 4

---
+++ Pclasses/Pclasses/frmMensalista.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Pclasses
+{
+    partial class frmMensalista
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtMensalista = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtSalMensal = new System.Windows.Forms.TextBox();
+            this.txtDataEntrada = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 270);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Instanciar mensalista";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(258, 270);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 59);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Instanciar mensalista passando parâmetros";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // txtMensalista
+            // 
+            this.txtMensalista.Location = new System.Drawing.Point(258, 36);
+            this.txtMensalista.Name = "txtMensalista";
+            this.txtMensalista.Size = new System.Drawing.Size(100, 26);
+            this.txtMensalista.TabIndex = 2;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(258, 81);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 26);
+            this.txtNome.TabIndex = 3;
+            // 
+            // txtSalMensal
+            // 
+            this.txtSalMensal.Location = new System.Drawing.Point(258, 132);
+            this.txtSalMensal.Name = "txtSalMensal";
+            this.txtSalMensal.Size = new System.Drawing.Size(100, 26);
+            this.txtSalMensal.TabIndex = 4;
+            // 
+            // txtDataEntrada
+            // 
+            this.txtDataEntrada.Location = new System.Drawing.Point(258, 180);
+            this.txtDataEntrada.Name = "txtDataEntrada";
+            this.txtDataEntrada.Size = new System.Drawing.Size(100, 26);
+            this.txtDataEntrada.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Mensalista";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Nome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Salário Mensal";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(217, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Data de entrada na empresa:";
+            // 
+            // frmMensalista
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDataEntrada);
+            this.Controls.Add(this.txtSalMensal);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtMensalista);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "frmMensalista";
+            this.Text = "frmMensalista";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtMensalista;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtSalMensal;
+        private System.Windows.Forms.TextBox txtDataEntrada;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
